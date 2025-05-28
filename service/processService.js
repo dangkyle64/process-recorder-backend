db6db29105bb21f45b35755c1a68c06eb8530e89
dangkyle64 <156500580+dangkyle64@users.noreply.github.com>
2025-05-28 09:07:25 -0700

    set up all the basic CRUD services with try/catch blocks to make sure there are no silent errors
    author: dangkyle64
    date: 5/27/2025

--- service/processService.js
+++ service/processService.js
@@ -3,8 +3,68 @@ class ProcessService {
         this.ProcessModel = ProcessModel;
     };
 
-    async getAllProcesses() {
-        const processes = await this.ProcessModel.findAll();
-        return processes;
+    async getAll() {
+        try {
+            const processes = await this.ProcessModel.findAll();
+            return processes;
+        } catch(error) {
+            console.error('Error getting the processes in processService: ', error);
+            return;
+        };
+    };
+
+    async findOne(id) {
+        try {
+            const process = await this.ProcessModel.findOne({
+                where: { id: id },
+            });
+
+            return process;
+        } catch(error) {
+            console.error('Error getting the process in processService: ', error);
+            return;
+        };
+    };
+
+    async createData(data) {
+        try {
+            const createdProcessData = await this.ProcessModel.create(data);
+            return createdProcessData;
+        } catch(error) {
+            console.error('Error creating the process in processService: ', error);
+            return;
+        };
+    };
+
+    async updateData(id, data) {
+        try {
+            const [affectedRows, updatedProcesses] = await this.ProcessModel.update(data, {
+                where : {id : id },
+                returning: true,
+                },
+            );
+
+            if (affectedRows === 0) {
+                throw new Error('Process not found');
+            };
+
+            return updatedProcesses[0];
+        } catch(error) {
+            console.error('Error creating the process in processService: ', error);
+            return;
+        };
+    };
+
+    async deleteData(id) {
+        try {
+            await this.ProcessModel.destroy({
+                where: { id: id },
+            });
+        } catch(error) {
+            console.error('Error creating the process in processService: ', error);
+            return;
+        };
     };
 };
+
+export default ProcessService;
