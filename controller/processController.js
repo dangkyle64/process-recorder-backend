bbff582dd52358b39b4604fa2341ad94826d05a9
dangkyle64 <156500580+dangkyle64@users.noreply.github.com>
2025-05-31 07:27:58 -0700

    connect the services for the getAll, findOne, create to the controller endpoint, set up the general endpoints in the router to be tested
    author: dangkyle64
    date: 5/31/2025

--- controller/processController.js
+++ controller/processController.js
@@ -5,6 +5,52 @@ class ProcessController {
 
     async getAllProcesses(request, response) {
         try {
+            const processes = await this.processService.getAll();
+            response.status(200).json(processes);
+        } catch(error) {
+            console.error('Error: ', error);
+            response.status(500).json({ 'message': 'Error getting the processes' });
+        };
+    };
+
+    async getProcess(request, response) {
+
+        const id = request.params.id;
+
+        try {
+            const process = await this.processService.findOne(id);
+            response.status(200).json(process);
+        } catch(error) {
+            console.error('Error: ', error);
+            response.status(500).json({ 'message': 'Error getting the process' });
+        };
+    };
+
+    async createProcess(request, response) {
+
+        const { name, steps } = request.body;
+
+        try {
+            const process = await this.processService.createData(name, steps);
+            response.status(200).json(process);
+        } catch(error) {
+            console.error('Error: ', error);
+            response.status(500).json({ 'message': 'Error getting the URLs' });
+        };
+    };
+
+    async updateProcess(request, response) {
+        try {
+            // await processService.getAllObjects etc 
+            response.status(200).json({});
+        } catch(error) {
+            console.error('Error: ', error);
+            response.status(500).json({ 'message': 'Error getting the URLs' });
+        };
+    };
+
+    async deleteProcess(request, response) {
+        try {
             // await processService.getAllObjects etc 
             response.status(200).json({});
         } catch(error) {
--- router/processRouter.js
+++ router/processRouter.js
@@ -5,9 +5,9 @@ import { processController } from '../controller/initializeController.js';
 const router = express.Router();
 
 router.get('/process', processController.getAllProcesses.bind(processController));
-//router.get
-//router.post
-//router.put
-//router.delete
+router.get('/process/:id', processController.getProcess.bind(processController));
+router.post('/process/', processController.createProcess.bind(processController));
+router.put('/process/:id', processController.updateProcess.bind(processController));
+router.delete('/process/:id', processController.deleteProcess.bind(processController));
 
 export default router;
